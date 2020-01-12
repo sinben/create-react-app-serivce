a807c70981fe9ee7ad548b5c13bb75cb7a3b571d
Sindre Benonisen <sbeno@equinor.com>
2020-01-12 14:41:18 +0100

    first commit

---
+++ Controllers/ImpController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using imp_service.Models;
+using imp_service.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace imp_service.Controllers
+{
+    [ApiController]
+  
+    public class ImpController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<ImpController> _logger;
+         private readonly ImpService _service;
+
+        public ImpController(ILogger<ImpController> logger)
+        {
+            _logger = logger;
+            _service = new ImpService();
+        }
+        [Route("/imps")]
+        [HttpGet]
+        public IEnumerable<Imp> Get()
+        {
+            return _service.GetImps();
+
+        }
+    }
+}
---
+++ Models/Imp.cs
@@ -0,0 +1,8 @@
+namespace imp_service.Models {
+
+    public class Imp{
+        public int Id {get;set;}
+        public string Name {get;set;}
+        public string Url {get;set;}
+    }
+}
